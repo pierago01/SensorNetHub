b4ec32416ed81101bb57b34dcca819aab348e5b5
pierago01 <81223389+pierago01@users.noreply.github.com>
2024-02-08 11:32:32 +0100

    Made some changes

--- app.js
+++ app.js
@@ -30,15 +30,11 @@ var container = database.container("<container-name-here>");
 var indexRouter = require("./routes/index");
 var graficoRouter = require("./routes/grafico");
 
-// Monta le route sull'applicazione web
+
 app.use(expressLayout);
 app.use("/", indexRouter);
 app.use("/grafico", graficoRouter);
 
-/*Crea il listener del server
-app.listen(3001, function () {
-  console.log("Server in ascolto sulla porta 3000");
-})*/
 
 const PORT = 8080;
 app.listen(PORT, () => {
